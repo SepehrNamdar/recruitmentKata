8871101d1b1102ece722e96c8a6d119540d82b72
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2019-03-24 15:06:48 +0100

    refactor

--- src/main/java/common/DateUtils.java
+++ src/main/java/common/DateUtils.java
@@ -7,7 +7,7 @@ public class DateUtils {
 
     public static final LocalDateTime TODAY = LocalDateTime.now();
 
-    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+    private static final String YYYY_MM_DD = "yyyy-MM-dd";
 
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DateUtils.YYYY_MM_DD);
 }
--- src/test/java/recruitment/use_case/InterviewPlannerShould.java
+++ src/test/java/recruitment/use_case/InterviewPlannerShould.java
@@ -1,34 +1,55 @@
 package recruitment.use_case;
 
+import common.DateUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import recruitment.exposition.PlannerRequest;
 import recruitment.model.Interview;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class InterviewPlannerShould {
+    @Spy Interview interview;
+    @Mock CandidateRepository candidates;
+    @Mock RecruitersReferential recruitersReferential;
+
+    private static final LocalDateTime TODAY = DateUtils.TODAY;
 
     @Test
     void schedule_an_interview() {
-        Interview interview = spy(new Interview());
-        CandidateRepository candidates = mock(CandidateRepository.class);
-        RecruitersReferential recruitersReferential = mock(RecruitersReferential.class);
-        InterviewPlanner planner = new InterviewPlanner(interview, candidates, recruitersReferential);
-        PlannerRequest request = new PlannerRequest();
-        LocalDateTime today = LocalDateTime.now();
-        request.setDate(today);
-        CandidateData javaCandidate = new CandidateData();
-        when(candidates.findCandidateById(request.getCandidateId())).thenReturn(javaCandidate);
-        List<RecruiterData> recruiters = new ArrayList<>();
-        when(recruitersReferential.findCurrentMonthRecruiters()).thenReturn(recruiters);
+        PlannerRequest request = createPlannerRequest();
+        CandidateData javaCandidate = getCandidate(request);
+        List<RecruiterData> recruiters = getRecruiters();
 
+        InterviewPlanner planner = new InterviewPlanner(interview, candidates, recruitersReferential);
         planner.scheduleInterview(request);
 
-        verify(interview).plan(today, javaCandidate, recruiters);
+        verify(interview).plan(TODAY, javaCandidate, recruiters);
+    }
+
+    private List<RecruiterData> getRecruiters() {
+        List<RecruiterData> recruiters = new ArrayList<>();
+        when(recruitersReferential.findCurrentMonthRecruiters()).thenReturn(recruiters);
+        return recruiters;
+    }
+
+    private CandidateData getCandidate(PlannerRequest request) {
+        CandidateData javaCandidate = new CandidateData();
+        when(candidates.findCandidateById(request.getCandidateId())).thenReturn(javaCandidate);
+        return javaCandidate;
+    }
+
+    private PlannerRequest createPlannerRequest() {
+        PlannerRequest request = new PlannerRequest();
+        request.setDate(TODAY);
+        return request;
     }
 }
